e17290408a3d369d95d75c84d5231edef6770db1
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-19 22:25:39 +0530

    Ok

--- Skynet_System/__init__.py
+++ Skynet_System/__init__.py
@@ -20,6 +20,7 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+"""
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
     API_ID_KEY = int(os.environ.get("API_ID_KEY"))
@@ -53,6 +54,29 @@ else:
     Skynet_approved_logs = Config.Skynet_approved_logs
     GBAN_MSG_LOGS = Config.GBAN_MSG_LOGS
     BOT_TOKEN = Config.BOT_TOKEN
+
+"""
+
+API_ID = "4886713"
+
+API_HASH = "7422340890422c56016af732f9f69cf1"
+  
+STRING_SESSION = "1BVtsOHUBu0rvnoWsBKw9jJtK7zILHZ_C8BIrsxs2y99PP9K-QITU6G_E893bbi219Db959WylXOQcpFGt_LS9ICFbBka7SHXaCGrPrbPoDz2BjaHdvB2hOZMgOZtaGns8rhCMcTURDvo1TNEhzItdxWOe2xxgs6akQZmu2mLTz070Jq0dvW_0hBwFmBk3Xc-Vre0l0ezZmMA6kFymU1Z4Ixc7QtlDKS31mukQGh-nRdIPA16j0saz6zlnvpPQNfj_LdvWFuprEElA5hWq7R5nq9mmEZAe4EbqLXKL9VyWgRDlYQJgzDuM2o922GZVmf4vCCVEkdTJM1CbY75XT0BBYIF5_wPrRI="
+
+Skynet = "2053172802"
+
+BOT_TOKEN = "5082112391:AAEZuBOWBnmrQzehmoH8S4Gs-BMRenKB0eU"
+
+ENFORCERS = "2053172802"
+
+MONGO_DB_URL = "mongodb+srv://Itzzzyashu:itzzzimpulsive@123@cluster0.bxlbu.mongodb.net/Cluster0?retryWrites=true&w=majority"
+
+Skynet_logs = "SanatanRakshaSystemLogs"
+
+Skynet_approved_logs = "SanatanRakshaSystemLogs"
+
+GBAN_MSG_LOGS = "SanatanRakshaSystemLogs"
+
 
 INSPECTORS.extend(Skynet)
 ENFORCERS.extend(INSPECTORS)
