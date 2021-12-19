1b28dddce2df9a55c8e30bbd08486735c94136d4
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-20 01:52:21 +0530

    Ok

--- Skynet_System/plugins/whois.py
+++ Skynet_System/plugins/whois.py
@@ -23,7 +23,13 @@ async def whois(event):
         return
     await System.send_message(
         event.chat_id,
-        f"╒═══「 Appraisal results: 」\n❂ First Name: `{data.user.last_name}`\n❂ Last Name: `{data.user.first_name}`\n❂ Username: @{data.user.username}\n❂ Userlink: [{data.user.first_name}](tg://user?id={data.user.id})\n❂ User ID: `{data.user.id}`\n❂ About: {data.about}",
+        f"╒═══「 **Appraisal results** 」",
+        f"\n**• First Name:** `{data.user.last_name}`",
+        f"\n**• Last Name:** `{data.user.first_name}`",
+        f"\n**• Username:** @{data.user.username}",
+        f"\n**• Userlink:** [{data.user.first_name}](tg://user?id={data.user.id})",
+        f"\n**• User ID:** `{data.user.id}`",
+        f"\n**• About:** {data.about}",
     )
 
 
