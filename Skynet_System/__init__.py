f7806495b8f20ebbe90f799aaa33c448cf939ad9
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-19 22:33:49 +0530

    Ok

--- Skynet_System/__init__.py
+++ Skynet_System/__init__.py
@@ -63,13 +63,13 @@ API_HASH = "7422340890422c56016af732f9f69cf1"
   
 STRING_SESSION = "1BVtsOHUBu0rvnoWsBKw9jJtK7zILHZ_C8BIrsxs2y99PP9K-QITU6G_E893bbi219Db959WylXOQcpFGt_LS9ICFbBka7SHXaCGrPrbPoDz2BjaHdvB2hOZMgOZtaGns8rhCMcTURDvo1TNEhzItdxWOe2xxgs6akQZmu2mLTz070Jq0dvW_0hBwFmBk3Xc-Vre0l0ezZmMA6kFymU1Z4Ixc7QtlDKS31mukQGh-nRdIPA16j0saz6zlnvpPQNfj_LdvWFuprEElA5hWq7R5nq9mmEZAe4EbqLXKL9VyWgRDlYQJgzDuM2o922GZVmf4vCCVEkdTJM1CbY75XT0BBYIF5_wPrRI="
 
-Skynet = "2053172802"
-
 BOT_TOKEN = "5082112391:AAEZuBOWBnmrQzehmoH8S4Gs-BMRenKB0eU"
 
-ENFORCERS = "2053172802"
-
-INSPECTORS = "2053172802"
+with open(os.path.join(os.getcwd(), "Skynet_System/elevated_users.json"), "r") as f:
+        data = json.load(f)
+    Skynet = data["Skynet"]
+    ENFORCERS = data["ENFORCERS"]
+    INSPECTORS = data["INSPECTORS"]
 
 MONGO_DB_URL = "mongodb+srv://Itzzzyashu:itzzzimpulsive@123@cluster0.bxlbu.mongodb.net/Cluster0?retryWrites=true&w=majority"
 
@@ -81,6 +81,7 @@ GBAN_MSG_LOGS = "SanatanRakshaSystemLogs"
 
 
 INSPECTORS.extend(Skynet)
+
 ENFORCERS.extend(INSPECTORS)
 
 session = aiohttp.ClientSession()
