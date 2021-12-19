1cb1a995c290895ac2aa7a9c12ca53c05363d268
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-20 01:46:33 +0530

    Ok

--- Skynet_System/strings.py
+++ Skynet_System/strings.py
@@ -1,5 +1,5 @@
 on_string = """
-╒═══「 Skynet X System • Server Core 」
+╒═══「 Sanatan-Raksha-System • Server Core 」
 ➖➖➖➖➖➖➖➖➖
 ➛ NAME: {name}
 ➛ RANK: {Enforcer}
@@ -11,20 +11,20 @@ on_string = """
 # If you still wanna change it change the regex too
 scan_request_string = """
 ╒═══「 $SCAN 」
-Skynet X Scan request!
-**❂ Enforcer:** {enforcer} 
-**❂ User scanned:** {spammer}
-**❂ Reason:** `{reason}`
-**❂ Scan Source:** {chat}
-**❂ Target Message:** `{message}`
+Sanatan-Raksha-Scan request!
+**• Enforcer:** {enforcer} 
+**• User scanned:** {spammer}
+**• Reason:** `{reason}`
+**• Scan Source:** {chat}
+**• Target Message:** `{message}`
 """
 forced_scan_string = """
 ╒═══「 $FORCED 」
-**❂ Inspector:** {ins}
-**❂ Target:** {spammer}
-**❂ Reason:** `{reason}`
-**❂ Scan Source:** {chat}
-**❂ Target Message:** `{message}`
+**• Inspector:** {ins}
+**• Target:** {spammer}
+**• Reason:** `{reason}`
+**• Scan Source:** {chat}
+**• Target Message:** `{message}`
 """
 
 reject_string = """
@@ -42,19 +42,19 @@ proof_string = """
          ┗━[DelDog]({url})"""
 
 scan_approved_string = """
-╒═══「 #LethalEliminator 」
-**❂ Target User:** {scam}
-**❂ Crime Coefficient:** `Over 300`
-**❂ Reason:** `{reason}`
-**❂ Enforcer:** `{enforcer}`
-**❂ Case Number:** `{proof_id}`
+╒═══「 #ApprovedSystemScan 」
+**• Target User:** {scam}
+**• Crime Coefficient:** `Over 300`
+**• Reason:** `{reason}`
+**• Enforcer:** `{enforcer}`
+**• Case Number:** `{proof_id}`
 """
 
 bot_gban_string = """
 ╒═══「 #DestroyDecomposer 」
-**❂ Enforcer:** `{enforcer}`
-**❂ Target User:** {scam}
-**❂ Reason:** `{reason}`
+**• Enforcer:** `{enforcer}`
+**• Target User:** {scam}
+**• Reason:** `{reason}`
 """
 
 # https://psychopass.fandom.com/wiki/Crime_Coefficient_(Index)
