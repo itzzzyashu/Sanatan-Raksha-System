d0c76c42f7ad23ff138a50b535a71c27b596d80c
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-22 23:36:32 +0530

    Add files via upload

---
+++ Sanatan_Raksha_System/plugins/safe.py
@@ -0,0 +1,30 @@
+from Skynet_System import System, system_cmd
+import os
+import sys
+import subprocess
+
+
+@System.on(system_cmd(pattern=r"SRS gitpull"))
+async def gitpull(event):
+    subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
+    await event.reply("Git pulled probably.")
+    os.system("restart.bat")
+    os.execv("start.bat", sys.argv)
+
+
+@System.on(system_cmd(pattern=r"SRS restart"))
+async def reboot(event):
+    if event.fwd_from:
+        return
+    await event.reply("Restarting.....")
+    await System.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    sys.exit()
+
+
+@System.on(system_cmd(pattern=r"SRS shutdown"))
+async def shutdown(event):
+    if event.fwd_from:
+        return
+    await event.reply("Shutting Down... ")
+    await System.disconnect()
---
+++ Sanatan_Raksha_System/plugins/whois.py
@@ -0,0 +1,37 @@
+from telethon.tl.functions.users import GetFullUserRequest
+from Skynet_System import System, system_cmd
+
+
+@System.on(system_cmd(pattern=r"whois"))
+async def whois(event):
+    try:
+        to_get = event.pattern_match.group(1)
+    except Exception:
+        if event.reply:
+            replied = await event.get_reply_message()
+            to_get = int(replied.sender.id)
+        else:
+            return
+    try:
+        to_get = int(to_get)
+    except Exception:
+        pass
+    try:
+        data = await System(GetFullUserRequest(to_get))
+    except Exception:
+        await event.reply("Failed to get data of the user")
+        return
+    await System.send_message(
+        event.chat_id,
+        f"╒═══「 Appraisal results: 」\n❂ First Name: `{data.user.last_name}`\n❂ Last Name: `{data.user.first_name}`\n❂ Username: @{data.user.username}\n❂ Userlink: [{data.user.first_name}](tg://user?id={data.user.id})\n❂ User ID: `{data.user.id}`\n❂ About: {data.about}",
+    )
+
+
+help_plus = """ Here is Help for **Whois** -
+• `whois` - get data of the user.
+• `uid` - get User ID.
+**Notes:**
+`/` `?` `.` `!` are supported prefixes.
+**Example:** `/addenf` or `?addenf` or `.addenf`
+"""
+__plugin_name__ = "whois"
