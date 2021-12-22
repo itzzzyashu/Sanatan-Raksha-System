cd771c280ed01f368086cc56529c59cb00e2e69d
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-22 23:00:22 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Skynet_System
-:: Enables virtual env mode and then starts Skynet_System
-env\scripts\activate.bat && py -m Skynet_System
+TITLE Sanatan_Raksha_System
+:: Enables virtual env mode and then starts Sanatan_Raksha_System
+env\scripts\activate.bat && py -m Sanatan_Raksha_System
