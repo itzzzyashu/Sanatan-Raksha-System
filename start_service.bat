e331ef4de47205d835fa7c1e55826caf4df2e8c2
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-22 22:59:06 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Skynet_System
-net start Skynet_System
+net stop Sanatan-Raksha-System
+net start Sanatan-Raksha-System
